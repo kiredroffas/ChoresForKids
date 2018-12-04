6357362415f26b88f892e513824356ba628d4006
Erik <erik.safford@wsu.edu>
2018-12-03 19:01:41 -0800

    Finished implementing Chore List functionality

--- app/imports/ui/pages/list-stuff-page.js
+++ app/imports/ui/pages/list-stuff-page.js
@@ -1,12 +1,1 @@
 import { Template } from 'meteor/templating';
-import { Stuff } from '../../api/stuff/stuff.js';
-
-Template.List_Stuff_Page.helpers({
-
-  /**
-   * @returns {*} All of the Stuff documents.
-   */
-  stuffList() {
-    return Stuff.find();
-  },
-});
